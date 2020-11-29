5234b035819b66c7605639f2c1b1c0506a777050
Azhar Ahamed <azharahamed@azhars-mbp.home>
2020-11-29 10:49:52 -0500

    Learning Spring Annotations

---
+++ src/main/java/com/udacity/jwdnd/ci/review/ReviewApplication.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.ci.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+@Configuration
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	public String basicMessage(){
+		System.out.println("Inside basic message");
+		return "Hello";
+	}
+
+	@Bean
+	public String compoundMessage(String basicMessage){
+		System.out.println("Inside compoundMessage, received : "+basicMessage);
+		return basicMessage + ", Spring!";
+	}
+
+	@Bean
+	public int characterCount(String message){
+		System.out.println("Inside the characterCount! ");
+		return message.length();
+	}
+}
