12c785401989a93857a6b5d7821a5da8c2c40233
Azhar Ahamed <azharahamed@azhars-mbp.home>
2020-12-10 00:49:36 -0500

    Thymeleaf

--- src/main/java/com/udacity/jwdnd/ci/review/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/ci/review/controller/HomeController.java
@@ -2,18 +2,34 @@ package com.udacity.jwdnd.ci.review.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.Instant;
+
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @RequestMapping("/home")
-    public String getHomePage(Model model){
-        model.addAttribute("welcomeMessage","Testing");
+
+    private MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping()
+    public String lowFive(MessageForm messageForm, Model model) {
+        messageListService.addMessage("low five.");
+        model.addAttribute("greetings", messageListService.getMessages());
         return "home";
     }
 
-    @RequestMapping("/")
-    public String getHome(Model model){
-        return getHomePage(model);
+    @PostMapping()
+    public String highFive(MessageForm messageForm, Model model) {
+        messageListService.addMessage("high five, " + messageForm.getText() + "!");
+        model.addAttribute("greetings", messageListService.getMessages());
+        return "home";
     }
 }
---
+++ src/main/java/com/udacity/jwdnd/ci/review/controller/MessageListService.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.ci.review.controller;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MessageListService {
+    private List<String> messages;
+
+    @PostConstruct
+    public void postConstruct(){
+        this.messages = new ArrayList<String>();
+    }
+
+    public void addMessage(String message){
+        messages.add(message);
+    }
+
+    public List<String> getMessages() {
+        return new ArrayList<>(this.messages);
+    }
+}
